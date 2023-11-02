1e506f9e608a8dc853f9acb01e7b2c870d0bd9bf
frecklehead <prashamsa35np@gmail.com>
2023-11-02 10:37:47 +0545

    1

--- recursion/c5.cpp
+++ recursion/c5.cpp
@@ -1,30 +1,19 @@
 #include<iostream>
-
 using namespace std;
-bool isPalindrome (string s){
-    
-    for( int i=0; i< s.length();i++){
-        if((s[i]<'A'|| s[i]>'Z')&&(s[i]<'a'|| s[i]>'z')&& (s[i]<48 || s[i]<57 ) ){
-            s.erase(i,1);
-            i--;
-        }
-    
-    else if ( s[i]>='A' && s[i]<='z'){
-        s[i]=s[i]+32;
-    }}
-     if( i>=s.length()/2) return true;
-     if( s[i]!= s[s.length()-1-i]) return false;
-     i++;
-     return isPalindrome(s);
-    
+int main (){
+int n;
+cin>>n;
+int arr[n];
+for ( int i=0; i<n;i++) cin>> arr[i];
 
+int hashh[10000] ={0};
+for( int i=0; i<n ;i++){
+    hashh[arr[i]] +=1;
 }
-int main(){
-   int  i=0;
- 
-    string s;
-    cout<< "enter your string";
-    cin>>s;
-    isPalindrome(s);
+int number;
+cout<<"want the frequency of";
+cin>>number;
+cout<<hashh[number];
+
 
 }
---
+++ recursion/c6.cc
@@ -0,0 +1,18 @@
+#include<iostream>
+using namespace std;
+int main(){
+string s;
+getline(cin,s);
+    int hashh[256]={0};
+    for ( int i=0;i<s.size();i++){
+       hashh[s[i]]++;
+    }
+    //fetch
+    char ch;
+    cout<<"character frequency";
+  cin >> ch;
+
+    cout<< hashh[ch];
+    
+     }
+  
