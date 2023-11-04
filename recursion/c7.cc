4a3d8ca8a19c27ba9d7943f08593eb93c0170bd5
frecklehead <prashamsa35np@gmail.com>
2023-11-04 08:27:38 +0545

    1

--- recursion/c7.cc
+++ recursion/c7.cc
@@ -1,22 +1,47 @@
+
+/*stl- standard template library
+#include<bits/stdc++.h>
+algorithms
+functions
+coontainers
+iterators
+
+
+
+*/
+
 #include<iostream>
+#include<bits/stdc++.h>
 using namespace std;
-bool checke(vector<int>& nums) {
-        int n=nums.size();
-        int cnt=0;
-        for(int i=1;i<n;i++){
-            if(nums[i-1]>nums[i]){
-                cnt++;
-            }
-        }
-        if(nums[n-1]>nums[0]){
-            cnt++;
-        }
-        return cnt<=1;
+int main (){
+     pair <int ,int > p={1,3};
+    cout<<p.first <<p.second;
+    pair <int , int > arr[]={{1,3},{2,3}};
+    cout << arr[1].second;
+    
+    //vector is a container which is dynamic  in nature.
+    /*
+    arrays have a constant size. so 
+    you cannot change its size in the 
+    ,middle of the problem, so there is
+     the use  of vectors
+     we can always increse or decrease the sixze of the 
+     vectors
+     so for the data structure where we dont knpw the sixe of he
+     size of  data structure thasts where  we use vectors
+       
+       declaration: vector <datatyoe> variable naeme
+
+    */
+
+   vector<int > v;
+   v.push_back(1);
+   v.push_emplace_back(2);
+   vector<pair <int ,int>> vi;
+   vi.push_back({1,2});
+   vi.emplace_back(1,3);// you dont require to add the curly braces;
+   vector<int > pola (5 ,90);// (no of the indexes , value for index) {90,90,90,90,90l,90}
+vector <int >pola2(pola);//copy of pola;
+ 
     }
 
-int main(){
-int n;
-int arr[n];
-for( int i=0;i<n;i++) cin>>arr[i];
-checke(arr);
-}
