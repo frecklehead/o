5689a2756d8e6aac03b8edd0b3e3f04081c1c8ad
frecklehead <prashamsa35np@gmail.com>
2023-11-05 23:51:22 +0545

    3

--- hashing/countingfreq.cc
+++ hashing/countingfreq.cc
@@ -1,23 +1,24 @@
 #include<bits/stdc++.h>
 using namespace std;
 
-vector<int> countFrequency ( int n, int x, vector<int>& nums){
-vector<int> arr(x,0);
-for( int i=0; i<n;i++){
-    arr[nums[i]]++;
-}
+// vector<int> countFrequency ( int n, int x, vector<int>& nums){
+// vector<int> arr(x,0);
+// for( int i=0; i<n;i++){
+//     arr[nums[i]]++;
+// }
 
-for( int i=0;i<x;i++){
-    if(arr[i]!=0){
-        cout<<arr[i];
-    }
-}
-}
+// for( int i=0;i<x;i++){
+//     if(arr[i]!=0){
+//         cout<<arr[i];
+//     }
+// }
+// }
 int main (){
     int n;
     cin>>n;
     int arr[n];
     for( int i=0; i<n;i++) cin>>arr[i];
+    cout << (sizeof(arr)/sizeof(arr[0]));
  map<int ,int>mpp;
  for( int  i=0;i<n;i++){
     mpp[arr[i]]++;
---
+++ hashing/tempCodeRunnerFile.cc
@@ -0,0 +1,7 @@
+ map<int ,int>mpp;
+//  for( int  i=0;i<n;i++){
+//     mpp[arr[i]]++;
+//  }
+//  for( auto i : mpp){
+//     cout<< i.second;
+//  }
